5745b989142fb9f23e785b9b33ebd82c464b9ccd
Jakeline-xx <jakeline.santana.rocha@gmail.com>
2022-05-30 14:39:31 -0300

    add: criado interface da service do contato

--- src/main/java/service/IContatoService.java
+++ src/main/java/service/IContatoService.java
@@ -1,4 +1,18 @@
 package service;
 
+import dto.ContatoDto;
+import dto.UsuarioDto;
+
+import java.util.List;
+
 public interface IContatoService {
+    List<ContatoDto> obterListaDeContatosDoUsuario(String nomeUsuario);
+
+    void deletarContatoDoUsuario(String nomeUsuario, long contatoId); //throws UserNotFoundException, TodoNotFoundException;
+
+    void atualizarContato(ContatoDto contatoDto); //throws UserNotFoundException;
+
+    void salvarContato(ContatoDto contatoDto); //throws UserNotFoundException;
+
+    ContatoDto gerarContatoAleatorio(UsuarioDto usuarioDto);
 }
