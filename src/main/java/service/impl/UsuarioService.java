60abb7b43e0215a28d571d39f6639969d6ef3202
Jakeline-xx <jakeline.santana.rocha@gmail.com>
2022-06-01 16:33:06 -0300

    add: inicio da criacao da service de usuario

--- src/main/java/service/impl/UsuarioService.java
+++ src/main/java/service/impl/UsuarioService.java
@@ -1,4 +1,38 @@
 package service.impl;
 
-public class UsuarioService {
+import domain.model.UsuarioEntity;
+import domain.repository.IUsuarioRepository;
+import dto.UsuarioDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import service.IUsuarioService;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+public class UsuarioService implements IUsuarioService {
+
+    private final IUsuarioRepository repository;
+
+    public void salvarUsuario(UsuarioDto usuarioDto) {
+        UsuarioEntity usuarioEntity = new UsuarioEntity(usuarioDto.getNomeUsuario(), usuarioDto.getNome());
+        repository.save(usuarioEntity);
+    }
+
+    public List<UsuarioDto> obterTodos() {
+        return
+                repository
+                        .findAll()
+                        .stream()
+                        .map(user -> new UsuarioDto(user.getNomeUsuario(), user.getNome()))
+                        .collect(Collectors.toList());
+    }
+
+//    public UsuarioEntity encontrarUsuarioPeloNomeUsuario(String usuario) throws UserNotFoundException{
+//        return repository
+//                .findById(usuario)
+//                .orElseThrow(() -> new UserNotFoundException("Usuario nao encontrado."));
+//    }
 }
